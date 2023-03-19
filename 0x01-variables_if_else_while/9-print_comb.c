5d74d01d91910e65cb35a1ae6e041b31ebdebbfd
clencyc <clency2023@gmail.com>
2023-03-19 07:02:17 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Generates a random number and
+ * specifies whether the generated
+ * number is greater than 5, less than 6 or 0
+ * Return: 0
+ */
+int main(void)
+{
+int = i;
+for (i = 0; i <= 9; ++i)
+{
+putchar(i)
+}
+{
+putchar(',');
+putchar(' ');
+}
+++i;
+}
+putchar('\n');
+return (0);
+}
