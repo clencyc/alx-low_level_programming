bc2a4ac257a2ec32d7040c2a1d3d71f2e0e6629f
clencyc <clency2023@gmail.com>
2023-03-19 06:31:28 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Generates a random number and
+ * specifies whether the generated
+ * number is greater than 5, less than 6 or 0
+ * Return: 0
+ */
+int main(void)
+{
+char n;
+for (n = 'z'; n = 'a'; --n)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
