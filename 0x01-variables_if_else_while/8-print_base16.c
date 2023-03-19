fa69aeabb8604eb093b1babf91d912368749ea77
clencyc <clency2023@gmail.com>
2023-03-19 06:53:51 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,14 +12,15 @@ int main(void)
 {
 char c;
 int i;
-for (i = 0; i = 10; ++i)
+for (i = 0; i < 10; i++)
 {
-putchar(i);
+putchar(48 + i);
 }
+
 for (c = 'a'; c <= 'f'; ++c)
 {
 putchar(c);
 }
 putchar('\n');
 return (0);
-}	
+}
