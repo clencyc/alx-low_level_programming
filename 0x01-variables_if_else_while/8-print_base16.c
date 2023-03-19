53cacb9e64aadc01716d6752a4954b98968b3051
clencyc <clency2023@gmail.com>
2023-03-19 06:49:18 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Generates a random number and
+ * specifies whether the generated
+ * number is greater than 5, less than 6 or 0
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+int i;
+for (i = 0; i = 10; ++i)
+{
+putchar(i);
+}
+for (c = 'a'; c <= 'f'; ++c)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}	
