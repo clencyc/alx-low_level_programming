bed16c86e94d0d50af82e20667a37f99a697e11a
clencyc <clency2023@gmail.com>
2023-03-22 04:00:58 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: prints _putchar.
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+char word[10] = "_putchar";
+for (i = 0; word[i] != 0; i++)
+{
+_putchar(word[i]);
+}
+_putchar('\n');
+return (0);
+}
+
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print alphabet 10times
+ *
+ * Reaturn 0(success)
+ */
+void print_alphabet_x10(void)
+{
+char c;
+char d;
+for (d = c; c<= 10; c++)
+{
+for (d = 'a'; d<='z'; d++)
+{
+_putchar(d);
+}
+putchar('\n');
+}
+}
+
