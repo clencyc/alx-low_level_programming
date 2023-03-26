20261527d0693b3d113e48671541033f729337e9
clencyc <clency2023@gmail.com>
2023-03-26 13:02:47 -0700

    goodnight

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,25 +6,16 @@
  */
 void print_number(int n)
 {
+	unsigned int i = n;
+
 	if (n < 0)
 	{
-		_putchar('-');
-		if (n >= -9)
-			_putchar(-1 * n + '0');
-		else
-		{
-			print_number(-1 * (n / 10));
-			_putchar(-1 * (n % 10) + '0');
-		}
+		_putchar(45);
+		i = -1;
 	}
-	else
+	if (n / 10)
 	{
-		if (n  < 10)
-			_putchar(n + '0');
-		else
-		{
-			print_number(n / 10);
-			_putchar(n % 10 + '0');
-		}
+		print_number(i / 10);
 	}
+	_putchar(i % 10 + '0');
 }
