72dfbcdc29a14a2f8efb51958b6f6a1f1fac98af
clencyc <clency2023@gmail.com>
2023-03-26 12:50:44 -0700

    The problem of distinguishing prime numbers from composite numbers and of resolving the latter into their prime factors is known to be one of the most important and useful in arithmetic

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,19 +1,48 @@
-#include "main.h"
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - prints the largest prime factor of 612852475143
- * Return: always 0
+ * is_prime():- check if an integer is prime or not.
+ *@n:- is the integer that we check with the fct is_prime.
+ *Return: - of is_prime 0 is @n is not prime 1 if it's.
  */
+int is_prime(long n)
+{
+	int i;
 
+	for (i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0)
+			return (0);
+	}
+	return (1);
+}
+
+
+/**
+ * main  - main function.
+ *
+ * Return: 0 Success
+ */
 int main(void)
 {
-	unsigned long int i, n = 612852475143;
+	long MAX;
+	long i;
+	long Numb = 612852475143;
 
-	for (i = 3; i < 782849; i = i + 2)
+	if (is_prime(Numb))
+		MAX = Numb;
+	else
 	{
-		while ((n % 1 == 0) && (n != 1))
-			n = n / i;
+		for (i = 2; i <= Numb; i++)
+		{
+			while (Numb % i == 0)
+			{
+				Numb /= i;
+				if (is_prime(i))
+					MAX = i;
+			}
+		}
 	}
-	printf("%lu\n", n);
+
+	printf("%ld\n", MAX);
 	return (0);
 }
