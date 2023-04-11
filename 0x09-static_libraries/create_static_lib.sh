b2ca3782ba9ed6160eb0a817a71db9c84ba2e1c7
clencyc <clency2023@gmail.com>
2023-04-11 08:00:04 -0700

    Create a script called create_static_lib.sh that creates a static library called liball.a from all the .c files that are in the current directory.

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.C
-ar -rc liball.a *.o
-ranlib liball.a
+
+# Find all .c files in the current directory
+files=$(find . -name "*.c")
+
+# Compile the .c files into object files
+for file in $files
+do
+    gcc -c $file
+done
+
+# Create the library archive file
+ar rcs liball.a *.o
+
+# Clean up the object files
+rm *.o
+
