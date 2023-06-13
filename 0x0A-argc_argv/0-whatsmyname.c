f3065d3f54c43f4cf7d412f49cd7b9a9fed6030a
clencyc <clency2023@gmail.com>
2023-06-13 03:21:31 -0700

    program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,8 @@
  * main - prints the name of the program
  * @argc: number of arguments
  * @argv: array of arguments
- * __attribute__((unused)) : supresses unused warnings
- * Return: Always 0 (Success)
  *
+ * Return: Always 0 (Success)
  */
 
 int main(int argc __attribute__((unused)), char *argv[])
@@ -16,4 +15,3 @@ int main(int argc __attribute__((unused)), char *argv[])
 
 	return (0);
 }
-
